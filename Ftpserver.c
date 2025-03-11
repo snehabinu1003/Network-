c085765294834d09b11e7bfafdab2c2ea5df909d
Abhijith Krishna T R <86849076+mortalprotagonist@users.noreply.github.com>
2025-03-11 10:39:09 +0530

    Update Ftpserver.c

--- Ftpserver.c
+++ Ftpserver.c
@@ -6,94 +6,155 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-void main() {
+#define PORT 1025
+#define BUFFER_SIZE 1024
+
+void handle_client(int client_sock);
+
+int main() {
     int server_sock, client_sock;
     struct sockaddr_in server_addr, client_addr;
     socklen_t addr_size;
 
+    // Create server socket
     server_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (server_sock < 0) {
-        perror("Socket error\n");
+        perror("Socket error");
         exit(1);
     }
 
-    memset(&server_addr, '\0', sizeof(server_addr));
+    // Initialize server address
+    memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(1025);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.100");
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("172.16.9.9");
 
-    int n = bind(server_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if (n < 0) {
-        perror("Bind error\n");
+    // Bind the server socket
+    if (bind(server_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind error");
+        close(server_sock);
         exit(1);
     }
 
-    listen(server_sock, 1024);
+    // Listen for incoming connections
+    if (listen(server_sock, 1024) < 0) {
+        perror("Listen error");
+        close(server_sock);
+        exit(1);
+    }
 
+    printf("Server listening on port %d...\n", PORT);
+
+    // Accept client connection
     addr_size = sizeof(client_addr);
-    client_sock = accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
+    client_sock = accept(server_sock, (struct sockaddr *) &client_addr, &addr_size);
+    if (client_sock < 0) {
+        perror("Accept error");
+        close(server_sock);
+        exit(1);
+    }
 
-    char buffer[1024];
+    // Handle client request
+    handle_client(client_sock);
+
+    // Close server socket after client disconnects
+    close(server_sock);
+    return 0;
+}
+
+void handle_client(int client_sock) {
+    char buffer[BUFFER_SIZE];
     char filename[100];
     FILE *fptr;
-    while (1) {
-        bzero(buffer, 1024);
-        recv(client_sock, buffer, 1024, 0);
 
-        if (strcmp(buffer, "PUT") == 0) {
-            bzero(buffer, 1024);
-            recv(client_sock, buffer, 1024, 0);
-            strcpy(filename, buffer);
-            bzero(buffer, 1024);
-            fptr = fopen(filename, "w");
-            while (1) {
-                recv(client_sock, buffer, 1024, 0);
-                printf("buffer = %s\n", buffer);
-                if (strcmp(buffer, "END$") == 0)
-                    break;
-                fprintf(fptr, "%s", buffer);
-                bzero(buffer, 1024);
+    while (1) {
+        // Clear buffer
+        memset(buffer, 0, sizeof(buffer));
+
+        // Receive request from client
+        ssize_t recv_len = recv(client_sock, buffer, sizeof(buffer), 0);
+        if (recv_len <= 0) {
+            if (recv_len == 0) {
+                printf("Client disconnected\n");
+            } else {
+                perror("Recv error");
             }
-            printf("File %s received\n", filename);
+            break;
+        }
+
+        // Handle PUT request
+        if (strcmp(buffer, "PUT") == 0) {
+            memset(buffer, 0, sizeof(buffer));
+
+            // Receive filename from client
+            recv(client_sock, buffer, sizeof(buffer), 0);
+            strcpy(filename, buffer);
+            memset(buffer, 0, sizeof(buffer));
+
+            // Open file to write
+            fptr = fopen(filename, "w");
+            if (fptr == NULL) {
+                perror("File open error");
+                break;
+            }
+
+            // Receive file data
+            while (1) {
+                recv(client_sock, buffer, sizeof(buffer), 0);
+                if (strcmp(buffer, "END$") == 0) {
+                    break;
+                }
+                fprintf(fptr, "%s", buffer);
+                memset(buffer, 0, sizeof(buffer));
+            }
+
+            printf("File '%s' received successfully\n", filename);
             fclose(fptr);
         }
 
+        // Handle GET request
         else if (strcmp(buffer, "GET") == 0) {
-            bzero(buffer, 1024);
+            memset(buffer, 0, sizeof(buffer));
+
+            // Send server process ID
             sprintf(buffer, "%d", getpid());
+            send(client_sock, buffer, sizeof(buffer), 0);
+            memset(buffer, 0, sizeof(buffer));
+
+            // Receive filename from client
+            recv(client_sock, buffer, sizeof(buffer), 0);
+            strcpy(filename, buffer);
+memset(buffer, 0, sizeof(buffer));
+
+            // Open file to read
+            fptr = fopen(filename, "r");
+            if (!fptr) {
+                printf("File '%s' does not exist\n", filename);
+                strcpy(buffer, "404");
+                send(client_sock, buffer, sizeof(buffer), 0);
+            } else {
+                strcpy(buffer, "200");
+                send(client_sock, buffer, sizeof(buffer), 0);
+
+                // Send file data
+                while (fgets(buffer, sizeof(buffer), fptr)) {
+                    send(client_sock, buffer, sizeof(buffer), 0);
+                    memset(buffer, 0, sizeof(buffer));
+                }
+
+                // Send END signal
+                strcpy(buffer, "END$");
+                send(client_sock, buffer, sizeof(buffer), 0);
+                printf("File '%s' sent successfully\n", filename);
+                fclose(fptr);
+            }
+        }
+
+        // Handle BYE request (close connection)
+        else if (strcmp(buffer, "BYE") == 0) {
+            close(client_sock);
+            printf("Connection closed\n");
+            break;
         }
     }
-}
-send(client_sock, buffer, 1024, 0);
-bzero(buffer, 1024);
-recv(client_sock, buffer, 1024, 0);
-strcpy(filename, buffer);
-
-bzero(buffer, 1024);
-fptr = fopen(filename, "r");
-if (!fptr) {
-    printf("File %s does not exist\n", filename);
-    strcpy(buffer, "404");
-    send(client_sock, buffer, 1024, 0);
-}
-
-else {
-    strcpy(buffer, "200");
-    send(client_sock, buffer, 1024, 0);
-
-    while (!feof(fptr)) {
-        bzero(buffer, 1024);
-        fscanf(fptr, " %[^\n] ", buffer);
-        send(client_sock, buffer, 1024, 0);
-    }
-    bzero(buffer, 1024);
-    strcpy(buffer, "END$");
-    send(client_sock, buffer, 1024, 0);
-    fclose(fptr);
-    printf("File %s sent\n", filename);
-}
-
-else if (strcmp(buffer, "BYE") == 0) {
-    close(server_sock);
-    exit(0);
 }
